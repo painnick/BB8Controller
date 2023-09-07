454f8af816a1a980f85002df0da6dfaa38e387ea
painnick <painnick@gmail.com>
2023-09-08 00:25:32 +0900

    ADD. Bluetooth Serial

--- app/src/main/java/com/painnick/bb8controller/MainActivity.java
+++ app/src/main/java/com/painnick/bb8controller/MainActivity.java
@@ -1,14 +1,183 @@
 package com.painnick.bb8controller;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final String TAG = "Main";
+
+    static final String BLUETOOTH_NAME = "VR-Trainer";
+    // Get permission
+    static final String[] REQUIRED_PERMISSIONS = {
+            Manifest.permission.BLUETOOTH_CONNECT,
+            Manifest.permission.BLUETOOTH_SCAN
+    };
+    static final int REQUEST_ENABLE_BT = 1;
+    static final int REQUEST_SCAN_BT = 2;
+    UUID BT_MODULE_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"); // "random" unique identifier
+    BluetoothAdapter btAdapter;
+    Set<BluetoothDevice> pairedDevices;
+    ArrayAdapter<String> btArrayAdapter;
+    ArrayList<String> deviceAddressArray;
+    private final BroadcastReceiver btReceiver = new BroadcastReceiver() {
+        @SuppressLint("MissingPermission")
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Discovery has found a device. Get the BluetoothDevice
+                // object and its info from the Intent.
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE, BluetoothDevice.class);
+                assert device != null;
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+                btArrayAdapter.add(deviceName);
+                deviceAddressArray.add(deviceHardwareAddress);
+                btArrayAdapter.notifyDataSetChanged();
+            }
+        }
+    };
+    BluetoothSocket btSocket = null;
+    ConnectedThread connectedThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Show paired devices
+        btArrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+        deviceAddressArray = new ArrayList<>();
+//        listView.setAdapter(btArrayAdapter); // TODO
+
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(btReceiver, filter);
+
+        ActivityCompat.requestPermissions(MainActivity.this, REQUIRED_PERMISSIONS, 1);
+
+        // Enable bluetooth
+        BluetoothManager btManager = (BluetoothManager) getApplicationContext().getSystemService(BLUETOOTH_SERVICE);
+        btAdapter = btManager.getAdapter();
+        if (!btAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        } else {
+            fillPaired();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_ENABLE_BT) {
+            if (resultCode == RESULT_OK) {
+                fillPaired();
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "블루투스를 켜주세요.", Toast.LENGTH_SHORT).show();
+            }
+        } else if (requestCode == REQUEST_SCAN_BT) {
+            if (resultCode == RESULT_OK) {
+                // 연결 시도
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "블루투스 BB-8을 패어링 해주세요.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    protected void fillPaired() {
+        btArrayAdapter.clear();
+        if (deviceAddressArray != null && !deviceAddressArray.isEmpty()) {
+            deviceAddressArray.clear();
+        }
+        boolean found = false;
+        String foundAddress = null;
+
+        pairedDevices = btAdapter.getBondedDevices();
+        if (pairedDevices.size() > 0) {
+            // There are paired devices. Get the name and address of each paired device.
+            for (BluetoothDevice device : pairedDevices) {
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+                if (BLUETOOTH_NAME.equals(deviceName)) {
+                    found = true;
+                    foundAddress = deviceHardwareAddress;
+                }
+                btArrayAdapter.add(deviceName);
+                deviceAddressArray.add(deviceHardwareAddress);
+            }
+        }
+        if (found) {
+            BluetoothDevice device = btAdapter.getRemoteDevice(foundAddress);
+
+            boolean connected = false;
+            try {
+                btSocket = createBluetoothSocket(device);
+                btSocket.connect();
+                connected = true;
+            } catch (IOException e) {
+//                flag = false;
+//                textStatus.setText("connection failed!");
+                e.printStackTrace();
+            }
+
+            if (connected) {
+                connectedThread = new ConnectedThread(btSocket);
+                connectedThread.start();
+            }
+        } else {
+            Toast.makeText(this, "블루투스 BB-8을 패어링 해주세요.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private BluetoothSocket createBluetoothSocket(BluetoothDevice device) throws IOException {
+        try {
+            final Method m = device.getClass().getMethod("createInsecureRfcommSocketToServiceRecord", UUID.class);
+            return (BluetoothSocket) m.invoke(device, BT_MODULE_UUID);
+        } catch (Exception e) {
+            Log.e(TAG, "Could not create Insecure RFComm Connection", e);
+        }
+        return device.createRfcommSocketToServiceRecord(BT_MODULE_UUID);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Don't forget to unregister the ACTION_FOUND receiver.
+        unregisterReceiver(btReceiver);
     }
 }
